bad6e2eb15c27aeb980ca9d696425c1952027ffb
Dan MacKinlay <myopenid@email.possumpalace.org>
2017-10-18 01:27:07 +1100

    metadata in setup.py

--- setup.py
+++ setup.py
@@ -10,3 +10,22 @@ if sys.platform == "darwin":
     compile_command += " -undefined dynamic_lookup"
 
 os.system(compile_command)
+setup(
+    name='AutoGP',
+    version='0.1',
+    description='Unified tool for automatric Gaussian PRocess Inference',
+    author='Karl Krauth and Edwin Bonilla',
+    author_email='edwinbonilla+autogp@gmail.com',
+    url='https://github.com/ebonilla/AutoGP',
+    license='Apache',
+    packages=find_packages('autogp'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
